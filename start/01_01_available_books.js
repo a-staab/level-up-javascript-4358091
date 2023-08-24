bb424b2f3d98278a596b2142ed1723a5584d3c65
a-staab <a-staab@users.noreply.github.com>
2023-08-24 00:37:36 +0000

    Pending changes exported from your codespace

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,26 @@
-// Write your code here
+class Book {
+  constructor(title, author, ISBN, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies;
+  }
+
+  get availability() {
+    if (this.numCopies == 0) return "out of stock";
+    else if (this.numCopies < 10) return "low stock";
+    else return "in stock";
+  }
+
+  sell(numSold = 1) {
+    this.numCopies -= numSold;
+  }
+
+  restock(numCopies = 5) {
+    this.numCopies += numCopies;
+  }
+
+};
+
+
+
